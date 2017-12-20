06a1d0250d7efd47803100dcd819a2db53d64e3c
Salvatore Ventrone <ventrosky@gmail.com>
2017-12-20 12:55:28 +0100

    Update Dog.js

--- dachshund/js/Dog.js
+++ dachshund/js/Dog.js
@@ -10,7 +10,7 @@ DogGame.Dog = function(game, x, y, key, paddle){
    
 	Phaser.Sprite.call(this, game, x, y, key);
 	
-	this.animations.add('walking',[1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],100,false);
+	this.animations.add('walking',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],100,false);
 	this.anchor.setTo(0.5);
 	this.game.physics.arcade.enable(this);
 
