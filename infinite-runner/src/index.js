21becabae68cd97e653f4d136bd5cc777e9750cb
ventrosky <ventrosky@develer.com>
2020-09-06 10:24:35 +0200

    infinite-jumper: eslint and game scene

--- infinite-runner/src/index.js
+++ infinite-runner/src/index.js
@@ -1,13 +1,13 @@
-import Phaser from "phaser";
-import Game from './scenes/Game.js'
+import Phaser from 'phaser';
+import Game from './scenes/Game';
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: 'phaser-example',
   width: 480,
   height: 640,
   scene: Game,
 };
 
+// eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
-
--- infinite-runner/src/scenes/Game.js
+++ infinite-runner/src/scenes/Game.js
@@ -1,28 +1,31 @@
-import Phaser from "phaser";
-import carrotImg from "../assets/carrot.png";
-import backgroundImg from "../assets/bg_layer1.png";
+import Phaser from 'phaser';
+import carrotImg from '../assets/carrot.png';
+import backgroundImg from '../assets/bg_layer1.png';
+import platformImg from '../assets/ground_grass.png';
 
-export default class Game extends Phaser.Scene{
-    constructor(){
-        super('game');
-    }
-    
-    preload() {
-        this.load.image('background', backgroundImg);
-        this.load.image("carrot", carrotImg);
-    }
-  
-    create() {
-        this.add.image(240, 320, 'background')
-        const carrot = this.add.image(240, 240, "carrot");
-  
-        this.tweens.add({
-            targets: carrot,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super('game');
+  }
+
+  preload() {
+    this.load.image('background', backgroundImg);
+    this.load.image('platform', platformImg);
+    this.load.image('carrot', carrotImg);
+  }
+
+  create() {
+    this.add.image(240, 320, 'background');
+    this.add.image(240, 320, 'platform').setScale(0.5);
+    const carrot = this.add.image(240, 240, 'carrot');
+
+    this.tweens.add({
+      targets: carrot,
+      y: 450,
+      duration: 2000,
+      ease: 'Power2',
+      yoyo: true,
+      loop: -1,
+    });
+  }
 }
